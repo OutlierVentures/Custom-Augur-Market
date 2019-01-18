5a1980b374252e88ae308ec7facdd5940a336ba8
Theo Turner <theo_turner@outlook.com>
2019-01-18 13:07:14 +0000

    Make market registration network-dynamic

--- reg.js
+++ reg.js
@@ -1,100 +1,109 @@
-var EthUtil = require('ethereumjs-util')
-var Augur = require('augur.js')
-var augur = new Augur()
+const network = process.env.NETWORK
+const account = process.env.ACCOUNT
+const key = process.env.KEY
 
-const account = process.env.ETH_ACCOUNT
-const privateKeyString = process.env.PRIVATE_KEY
-const privateKeyBuffer = EthUtil.toBuffer(privateKeyString)
+if (network && account && key) {
 
-var ethereumNode = { 
-  httpAddresses: [
-    process.env.ETHEREUM_HTTP
-  ],
-  wsAddresses: [
-    process.env.ETHEREUM_WS
-  ]
-  // ipc addresses can also be specified as:
-  // ipcAddresses: [ ... ]
+  console.log("\033[43mCreating market...\033[0m")
+  const EthUtil = require("ethereumjs-util")
+  const privateKeyBuffer = EthUtil.toBuffer(key)
+
+  const Augur = require("augur.js")
+  const augur = new Augur()
+
+  // Set config depending on network choice
+  var ethHTTP = "https://kovan.augur.net/ethereum-http"
+  var ethWS = "wss://kovan.augur.net/ethereum-ws"
+  var cashWrapper = "0xa265726f41b51bacb5d8e2907abceda9bc21b69b"
+  if (network == "main") {
+    ethHTTP = "https://mainnet.infura.io/augur"
+    ethWS = "wss://mainnet.infura.io/ws"
+    cashWrapper = "0xd5524179cb7ae012f5b642c1d6d700bbaa76b96b"
+  }
+
+  // Set node options
+  const ethereumNode = { 
+    httpAddresses: [
+      ethHTTP
+    ],
+    wsAddresses: [
+      ethWS
+    ]
+    // ipcAddresses: [ ... ]
+  }
+  const augurNode = "ws://127.0.0.1:9001"
+
+  // Connect to Ethereum & local Augur node
+  augur.connect({ ethereumNode, augurNode }, function (err, connectionInfo) {
+
+    //console.log(connectionInfo.ethereumNode.contracts.Universe)
+
+    /*
+    // Approve account
+    augur.accounts.approveAugur({
+      address: account, 
+      meta: {
+        accountType: "privateKey",
+        address: account,
+        signer: privateKeyBuffer,
+      },
+      onSent: function(result) { console.log(result) },
+      onSuccess: function(result) { console.log("Approval was successful.") },
+      onFailed: function(result) { console.log("Approval failed due to error:", result) }
+    })
+    */
+
+    // Create binary market
+    // Other market types: docs.augur.net/#create-market-functions
+    // ***************************************************************************
+    // SET MARKET OPTIONS BELOW
+    // ***************************************************************************
+    var _extraInfo = {
+      "resolutionSource": "https://www.nasdaq.com/symbol/msft",
+      "tags": [ "Stocks", "Microsoft" ],
+      "longDescription": ""
+    }
+    augur.createMarket.createYesNoMarket({
+      universe: connectionInfo.ethereumNode.contracts.Universe,
+      _endTime: 1577836800, // End time as unix epoch, use converter epochconverter.com
+      _feePerEthInWei: 1193046,
+      _denominationToken: cashWrapper,
+      _designatedReporterAddress: account,
+      _topic: "stocks",
+      _description: "Will Microsoft stock (MSFT) be below $50 at any point during 2019?",
+    // ***************************************************************************
+    // SET MARKET OPTIONS ABOVE
+    // ***************************************************************************
+      _extraInfo: JSON.stringify(_extraInfo),
+      meta: {
+        accountType: "privateKey",
+        address: account,
+        signer: privateKeyBuffer,
+      },
+      onSent: function (result) {
+        console.log(result)
+       },
+      onSuccess: function (result) {
+        console.log(result)
+        console.log("\033[42mMarket successfully created.\033[0m")
+        augur.augurNode.disconnect()
+        augur.disconnect()
+        process.exit()
+      },
+      onFailed: function (result) {
+        console.log(result)
+        console.log("\033[41mMarket creation failed. See error log above.\033[0m")
+        augur.augurNode.disconnect()
+        augur.disconnect()
+        process.exit()
+      }
+    })
+    
+  })
+
 }
-// To connect to a hosted Augur Node instead of a local Augur Node, substitute its WebSocket address below.
-var augurNode = "ws://127.0.0.1:9001" // local WebSocket address for an Augur Node
+else {
 
-// Attempt to connect to a local Ethereum node
-// If that fails, fall back to the hosted Ethereum node
-augur.connect({ ethereumNode, augurNode }, function (err, connectionInfo) {
+  console.log("\033[42mPlease set your NETWORK, ACCOUNT and KEY environment variables.\033[0m")
 
-  //console.log(connectionInfo.ethereumNode.contracts.Universe)
-
-  /*
-  // Approve account
-  augur.accounts.approveAugur({
-    address: account, 
-    meta: {
-      accountType: "privateKey",
-      address: account,
-      signer: privateKeyBuffer,
-    },
-    onSent: function(result) { console.log(result) },
-    onSuccess: function(result) { console.log("Approval was successful.") },
-    onFailed: function(result) { console.log("Approval failed due to error:", result) }
-  })
-  */
-
-  // Create binary market
-  var _extraInfo = {
-    "resolutionSource": "https://www.nasdaq.com/symbol/msft",
-    "tags": [ "Stocks", "Microsoft" ],
-    "longDescription": ""
-  }
-  augur.createMarket.createYesNoMarket({
-    universe: connectionInfo.ethereumNode.contracts.Universe,
-    _endTime: 1553108038,
-    _feePerEthInWei: 1193046,
-    _denominationToken: "0xd2ee83a8a2a904181ccfddd8292f178614062aa0",
-    _designatedReporterAddress: account,
-    _topic: "stocks",
-    _description: "Will Microsoft stock (MSFT) be below $50 at any point during 2018?",
-    _extraInfo: JSON.stringify(_extraInfo),
-    meta: {
-      accountType: "privateKey",
-      address: account,
-      signer: privateKeyBuffer,
-    },
-    onSent: function (result) { console.log(result) },
-    onSuccess: function (result) { console.log(result) },
-    onFailed: function (result) { console.log(result) }
-  })
-
-  /*
-  // Create categorical market
-  var _extraInfo = {
-    "resolutionSource": "By virtue of the fact that this market exists",
-    "tags": ["programming", "API"],
-    "outcomeNames": ["Yes", "No"],
-    "longDescription": ""
-  }
-  augur.createMarket.createCategoricalMarket({
-    universe: connectionInfo.ethereumNode.contracts.Universe,
-    _endTime: 1540875600,
-    _feePerEthInWei: 1193046,
-    _denominationToken: "0xd2ee83a8a2a904181ccfddd8292f178614062aa0",
-    _designatedReporterAddress: account,
-    _outcomes: ["Yes","No"],
-    _topic: "sports",
-    _description: "Can you create Kovan markets programatically?",
-    _extraInfo: JSON.stringify(_extraInfo),
-    meta: {
-      accountType: "privateKey",
-      address: account,
-      signer: privateKeyBuffer,
-    },
-    onSent: function (result) { console.log(result) },
-    onSuccess: function (result) { console.log(result) },
-    onFailed: function (result) { console.log(result) }
-  })
-  */
-  
-  // Disconnect
-  augur.disconnect()
-  
-})
+}
